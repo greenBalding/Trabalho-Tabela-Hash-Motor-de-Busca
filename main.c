f63f670da612055f6dae2b6ea78ea6c1e18fc383
Matheus Pains <matheuspains@pucgoias.edu.br>
2025-03-30 20:15:13 -0300

    add the text files folders to the project

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+// Trabalho – Tabela Hash – Motor de Busca
+// Alunos: ARTHUR CARDOSO MARQUES e MATHEUS SILVA PAINS
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+/*
+
+Objetivo: Criar um motor de busca de documentos por palavras-chave ordenados por relevância, tais como os 
+disponibilizados em sites de busca, como Google e Bing
+
+*/
+
